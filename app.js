f940966be5bfeb9daf4d353446b549963887faa0
johnvees <yohanesvelly23@gmail.com>
2022-06-20 15:43:54 +0700

    create view dashboard

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// router admin
+const adminRouter = require('./routes/admin');
+const adminController = require('./controllers/adminController');
+
 var app = express();
 
 // view engine setup
@@ -26,13 +30,16 @@ app.use(
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+//admin
+app.use('/admin', adminRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
