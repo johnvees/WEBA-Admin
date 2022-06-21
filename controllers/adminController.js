fd028b7718e970cd2b3549ae2c2fdd9830db4532
Yohanes Velly Sabattino <yohanesvelly23@gmail.com>
2022-06-21 15:46:59 +0700

    add view products

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,4 +2,8 @@ module.exports = {
   viewDashboard: (req, res) => {
     res.render('admin/dashboard/view_dashboard');
   },
+
+  viewProducts: (req, res) => {
+    res.render('admin/products/view_products');
+  },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/dashboard', adminController.viewDashboard);
+router.get('/products', adminController.viewProducts);
 
 module.exports = router;
